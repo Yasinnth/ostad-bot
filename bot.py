668285f92d35bc0ecf9262db28965dfa288a2667
Yasinnth <nodehimosa@gmail.com>
2026-08-23 23:01:01 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,7 +89,7 @@ async def get_specialty_text(message: Message, state: FSMContext):
     await state.update_data(specialty=message.text)
     await message.answer(
         "📚 نام دانشکده‌ای که در آن هستید چیست؟",
-        reply_markup=kb(["پزشکی", "داروسازی", "پرستاری", "مامایی", "➕ سایر"])
+        reply_markup=kb(["پزشکی","بهداشت حرفه ای", "داروسازی","علوم آزمایشگاهی", "پرستاری", "مامایی", "➕ سایر"])
     )
     await state.set_state(Review.faculty)
 
